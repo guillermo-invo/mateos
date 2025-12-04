4da4fe7fcc39291c81fe42e91d7b5732d158ef43
guille <guillermo@involucrate.uy>
2025-12-04 19:19:00 -0300

    [UI] Configure color palette and Hero UI theme

--- next-app/tailwind.config.js
+++ next-app/tailwind.config.js
@@ -1,13 +1,13 @@
-const {heroui} = require(' @heroui/theme');
+const {heroui} = require('@heroui/theme');
 
-/** @automatizaciones/src/types.ts {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/ @heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
